1765faa3de13ccbef014d8e27377c23c2356a7b5
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-07-21 23:42:16 +0300

    Implement crossout when input is clicked

--- src/displayList.js
+++ src/displayList.js
@@ -1,4 +1,6 @@
 const listItems = document.getElementById('listItems');
+const checkbox = document.getElementsByClassName('checkbox');
+
 const tasks = [
   {
     index: 1,
@@ -20,25 +22,44 @@ const tasks = [
 localStorage.setItem("tasks", JSON.stringify(tasks));
 const storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
-const clickCheckbox = () => {
-  if (document.querySelector('p').style.textDecoration = 'underline');
-}
-
-const listFunction = () => {
+const listFunction = (e) => {
   for (let i = 0; i < storedTasks.length; i += 1) {
     const list = document.createElement('li');
     const p = document.createElement('p');
+    p.className = 'paragraph'
+    p.id = `paragraph-${i}`
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.checked = true;
+    checkbox.id = `checkbox-${i}`;
+    checkbox.className = 'checkbox'
+    checkbox.checked = false;
+    checkbox.onclick = function(){
+      if (checkbox.checked) {
+        p.style.textDecoration = 'line-through';
+        tasks[i].completed = true
+        localStorage.clear()
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+      }
+      else {
+        p.style.textDecoration = 'none';
+        tasks[i].completed = false
+        localStorage.clear()
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+      }
+    };
     p.innerHTML = `${storedTasks[i].index} ${storedTasks[i].description} ${storedTasks[i].completed}`;
     listItems.appendChild(list);
     list.appendChild(checkbox);
     list.appendChild(p);
-    if (checkbox.checked == true) {
-      p.style.textDecoration = 'line-through';
-    }
   }
 };
+
+// const checkbox = document.getElementsByClassName('checkbox');
+// console.log(checkbox);
+// const p = document.querySelector('p');
+// const checked = () => {
+//   p.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+// }
+// checkbox.addEventListener('change', checked);
 
 export { listFunction as default };
--- src/updates.js
+++
@@ -1,7 +0,0 @@
-const updateValue = () => {
-  if (document.querySelector('input').checked == true) {
-    document.querySelector('p').style.textDecoration = 'line-through';
-  }
-}
-
-export { updateValue as default }
